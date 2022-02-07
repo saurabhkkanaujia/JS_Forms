4df967f72960d13e3675f1cc4c9faaab1682fdde
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-07 12:30:55 +0530

    Divided into Modules

--- src/script.js
+++ src/script.js
@@ -1,50 +1,80 @@
-function predictBMI(){
+function submitForm(){
+    var name = document.getElementById("name").value;
     var age = parseInt(document.getElementById("age").value);
     var wt = parseInt(document.getElementById("weight").value);
+
+    var output = "";
+    if (checkValues(name, age, wt)){
+        console.log("yes")
+        output = predictBMI(name, age, wt);
+    }
+    else {
+        output = "Error in Input Values";
+    }
+    display(output);
+}
+
+function checkValues(name, age, wt){
+    if (age == null || age == "" || age == " " || wt == null || wt == "" || wt == " " || name == null || name == "" || name == " " ){
+        return false;
+    }
+    else if (isNaN(age) || isNaN(wt) || typeof name != "string" || typeof parseInt(name) == "number"){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+
+function predictBMI(name, age, wt){
     var res = "";
-    if (age > 5 && age < 7){
+    if (age >= 5 && age <= 7){
         if (wt < 15){
-            res = "Underweight";
+            res = "Hello "+name+"!!! Your Weight is less than recommended value of 15kg at an age of "+age;
         }
-        else if (wt>=15 || wt <= 20){
-            res = "Perfect";
+        else if (wt>=15 && wt <= 20){
+            res = "Hello "+name+"!!! Your Weight is perfect";
         }
         else {
-            res = "Overweight";
+            res = "Hello "+name+"!!! Your Weight is greater than recommended value of 20kg at an age of "+age;
         }
     }
-    else if (age > 8 && age < 10){
+    else if (age >= 8 && age <= 10){
         if (wt < 21){
-            res = "Underweight";
+            res = "Hello "+name+"!!! Your Weight is less than recommended value of 21kg at an age of "+age;
         }
-        else if (wt>=21 || wt <= 25){
-            res = "Perfect";
+        else if (wt>=21 && wt <= 25){
+            res = "Hello "+name+"!!! Your Weight is perfect";
         }
         else {
-            res = "Overweight";
+            res = "Hello "+name+"!!! Your Weight is greater than recommended value of 25kg at an age of "+age;
         }
     }
-    else if (age > 11 && age < 15){
+    else if (age >= 11 && age <= 15){
         if (wt < 26){
-            res = "Underweight";
+            res = "Hello "+name+"!!! Your Weight is less than recommended value of 26kg at an age of "+age;
         }
         else if (wt>=26 || wt <= 30){
-            res = "Perfect";
+            res = "Hello "+name+"!!! Your Weight is perfect";
         }
         else {
-            res = "Overweight";
+            res = "Hello "+name+"!!! Your Weight is greater than recommended value of 30kg at an age of "+age;
         }
     }
     else if (age > 16 && age < 20){
-        if (wt < 26){
-            res = "Underweight";
+        if (wt < 31){
+            res = "Hello "+name+"!!! Your Weight is less than recommended value of 31kg at an age of "+age;
         }
-        else if (wt>=31 || wt <= 40){
-            res = "Perfect";
+        else if (wt>=31 && wt <= 40){
+            res = "Hello "+name+"!!! Your Weight is perfect";
         }
         else {
-            res = "Overweight";
+            res = "Hello "+name+"!!! Your Weight is greater than recommended value of 40kg at an age of "+age;
         }
     }
-    document.getElementById("result").innerHTML = "You are"+res;
+    return res;
+}
+function display(output){
+    document.getElementById("result").innerHTML = output;
 }
